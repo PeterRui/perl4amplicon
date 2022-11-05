52825dcb7e1778d7669c26521ee441f6ce993fb9
PeterRui <peter_rjp@163.com>
2022-11-05 21:06:14 +0800

    perltest

--- fastq2fasta.pl
+++ fastq2fasta.pl
@@ -1,22 +1,22 @@
 #!/usr/bin/perl -w
 print "Copyright: Junpeng Rui, Lanzhou University. peter_rjp\@163.com\n";
-print "This script is used to transfer fastq to fasta files.\n";
+print "This script is used to convert fastq to fasta files.\n";
 die "Usage:perl fastq2fasta.pl [the input file]\n" unless @ARGV>0;
 
 my $fastq_file = $ARGV[0];
 my $fasta_file = $fastq_file;
-open TMP, "$fastq_file" || die "Can't open fastq $fastq_file!\n";    
+open TMP, "$fastq_file" || die "Cannot open fastq $fastq_file!\n";    
 $fasta_file =~ s/fastq$|fq$/fasta/; 
-open MYOUT, ">$fasta_file"|| die "Can't open fasta $fasta_file"."_id!\n";
+open MYOUT, ">$fasta_file"|| die "Cannot open fasta $fasta_file"."_id!\n";
 my ($li,$nl);
+
 while($li=<TMP>){
-$nl++;
-if ($nl % 4 == 1) { # Illumina fastq contains 4 rows per seq. The qual data may also include @.
-print MYOUT $li if $li =~ s/^\@/\>/; 
-} 
-elsif ($nl % 4 == 2) {
-print MYOUT $li;  
-}
+	$nl++;
+	if ($nl % 4 == 1) { # Illumina fastq contains 4 rows per seq. The qual data may also include @.
+		print MYOUT $li if $li =~ s/^\@/\>/; 
+	} elsif ($nl % 4 == 2) {
+		print MYOUT $li;  
+	}
 }
 close MYOUT;
 close TMP;
---
+++ rename_seqid_for_usearch.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl -w
+
+sub usage(){
+<<EOF
+This script is used to rename sequence IDs in fasta/fastq files, and add ";barcodelabel=<File name>;" after the IDs, which could be identified by USEARCH. 
+For example, the first ID in "S1.fasta" will named as "S1_1;barcodelabel=S1;", and the second ID will be "S1_2;barcodelabel=S1;".
+
+Parameters: 
+-i input files (e.g., "*.fasta" or "1.fq;2.fq;3.fq")
+-s suffix of output (default is ".fasta" or ".fastq")
+-d output dir (default "rename")
+-f format of input files? (default 0)
+	0 - fasta, 1 - fastq
+EOF
+}
+
+use File::Basename qw<basename dirname>;
+use File::Path;
+use Getopt::Std;
+getopts("i:s:d:f:",\%opts);
+
+my $inputs= $opts{i}; 
+my $suff  = $opts{s};
+my $dout  = $opts{d}; $dout = "rename" unless (defined($dout));
+my $format= $opts{f};
+print "Copyright: Junpeng Rui, Lanzhou University. peter_rjp\@163.com\n";
+die usage() unless ($opts{i});
+
+unless (-e $dout) {
+	mkpath($dout);
+}
+
+unless ($suff) {
+  if ($format) {
+	$suff=".fastq";
+  } else {
+	$suff=".fasta";
+  }
+}
+ 
+my @myfile;
+if ($inputs=~ /\*/) {  # wildcard mode
+  @myfile = glob $inputs;
+}
+else {  # file list mode
+  @myfile = split/;/,$inputs;
+}
+print "@myfile\n";
+
+my $begin=1;
+
+while(@myfile) {
+	my $input = shift(@myfile);
+	open MYIN, $input || die "Cannot open input file $input!\n";
+	my $prefix=basename $input;
+	$prefix=~ s/$suff$//i;
+	open MYOUT, ">$dout/$prefix$suff" || die "Cannot open the output file.\n";
+ 
+	my $n=0;
+	$n+=$begin;
+	my $nl=0;
+	if ($format) {	# fastq
+	  while($li=<MYIN>){
+		$nl++;
+		if ($nl % 4 == 1) {
+			$li= '@'."${prefix}_$n;barcodelabel=${prefix};\n";
+			$n++;
+		}
+		print MYOUT $li;
+	  }
+	} else {	# fasta
+	  while($li=<MYIN>){
+		if($li =~ /^\>/) {
+			$li= '>'."${prefix}_$n;barcodelabel=${prefix};\n";
+			$n++;
+		}
+		print MYOUT $li;
+	  }
+	}
+	close MYOUT;
+	close MYIN;
+}
+
+
